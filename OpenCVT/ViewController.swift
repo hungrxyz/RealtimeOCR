e4862cb9a3a3b3e05099df86c82b6c965b797ecc
Devxhkl <devxhkl@outlook.com>
2015-02-18 22:11:03 +0100

    Initial Commit

---
+++ OpenCVT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenCVT
+//
+//  Created by Zel Marko on 18/02/15.
+//  Copyright (c) 2015 Zel Marko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
